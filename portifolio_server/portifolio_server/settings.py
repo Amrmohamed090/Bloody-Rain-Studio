4be6cdde39395c20998b1a962b0175d97cdb1c52
Amr Mohemd <93833053+Amrmohamed090@users.noreply.github.com>
2024-05-09 14:58:29 +0300

    add to allowed hosts

--- portifolio_server/portifolio_server/settings.py
+++ portifolio_server/portifolio_server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-q9o(%wi_o0^*5al=8qe19g_t&dlg90wlorozm(9s$ay&=3(i)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite-k3q7.onrender.com"]
 
 
 # Application definition
