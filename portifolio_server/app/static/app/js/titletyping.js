7566bc23bbfad4bf16a202fb831c18bfb01c6367
BekoBeko <87792475+BekoBeko00@users.noreply.github.com>
2024-06-23 16:59:27 +0300

    almost final changes

--- portifolio_server/app/static/app/js/titletyping.js
+++ portifolio_server/app/static/app/js/titletyping.js
@@ -2,14 +2,6 @@ new TypeIt("#element", {
     strings: ["Welcome To", "<span>Bloody Rain</span>"],
     speed: 80,
     waitUntilVisible: true,
-    cursor: {
-      animation: {
-          options: {
-              iterations: Infinity,
-              easing: "linear",
-              fill: "forwards",
-          },
-          loop: false,
-      },
-  }
-  }).go();
+    cursor: false,
+}
+).go();
