26b98bd034ca22d47bd59becef8e909e21331c34
Amr Mohemd <93833053+Amrmohamed090@users.noreply.github.com>
2024-06-23 11:29:11 +0300

    check env variable for database

--- portifolio_server/portifolio_server/settings.py
+++ portifolio_server/portifolio_server/settings.py
@@ -136,7 +136,7 @@ ASGI_APPLICATION = 'portifolio_server.asgi.application'
 DATABASES = {
     'default': dj_database_url.config(       
          # Replace this value with your local database's connection string.        
-         default='postgres://portifolio_db_ot89_user:nOEfqsQs0CYRgZY36H4LOXzaJFy5SX36@dpg-cpp1ehuehbks73bq73fg-a/portifolio_db_ot89',        
+         default=f"{os.environ['DATABASE_URL']}",        
          conn_max_age=600    )
          }
 
