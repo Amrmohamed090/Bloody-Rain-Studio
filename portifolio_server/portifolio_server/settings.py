2a3d70a71ce7da2319ef163ba19dc8ae75b1764f
Amr Mohemd <93833053+Amrmohamed090@users.noreply.github.com>
2024-06-23 10:10:26 +0300

    debug = false

--- portifolio_server/portifolio_server/settings.py
+++ portifolio_server/portifolio_server/settings.py
@@ -50,7 +50,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q9o(%wi_o0^*5al=8qe19g_t&dlg90wlorozm(9s$ay&=3(i)j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ["mysite-k3q7.onrender.com","127.0.0.1", "portifolio-server-21uj.onrender.com","bloody-rain.onrender.com","bloodyrainstudio.com", "www.bloodyrainstudios.com"]
