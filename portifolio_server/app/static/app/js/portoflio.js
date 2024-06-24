45705b175be1b6a9bd63da9d39c635319d62cfa4
Amr Mohemd <93833053+Amrmohamed090@users.noreply.github.com>
2024-06-25 02:46:34 +0300

    misonary false

--- portifolio_server/app/static/app/js/portoflio.js
+++ portifolio_server/app/static/app/js/portoflio.js
@@ -1,16 +1,14 @@
-window.onload = function() {
-    
+document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll("#portfolio-flters li").forEach(function(t) {
         t.addEventListener("click", function() {
             var t, e = this.getAttribute("data-service-id"),
                 i = window.location.href;
             t = null !== e && "" !== e ? i.includes("?pageid=") ? i.replace(/(pageid=)[^&]+/, "$1" + e) : i + "?pageid=" + e : i.split("?")[0], window.history.replaceState({}, "", t)
         })
-    });
-
+    })
+}), $(document).ready(function() {
     var t, e, i = $(".portfolio-container").isotope({
             itemSelector: ".portfolio-item",
-            layoutMode: "masonry"
         }),
         o = (e = (t = location.search.match(/pageid=([^&]+)/i)) && t[1]) ? ".service" + e : "*";
     $('#portfolio-flters li[data-filter="' + o + '"]').addClass("active"), i.isotope({
@@ -19,5 +17,5 @@ window.onload = function() {
         $("#portfolio-flters li").removeClass("active"), $(this).addClass("active"), i.isotope({
             filter: $(this).data("filter")
         })
-    });
-};
+    })
+});
